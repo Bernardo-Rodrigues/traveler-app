fab45a9b0ceb13a7bd36eb30132846b1156648a6
Bernardo Rodrigues <bernardo.rodrigues06@unifei.edu.br>
2022-06-29 09:49:40 -0300

    chore: change clerk provider lcoation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, useNavigate } from "react-router-dom";
 import AppRoutes from "./routes";
 import Alert from "./shared/components/Alert";
 import GlobalProvider from "./shared/contexts/GlobalContext";
@@ -6,19 +6,29 @@ import { GlobalStyle } from "./shared/styles/globalStyle";
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "./shared/styles/themes";
 import { LoadScript } from "@react-google-maps/api";
+import { ClerkProvider } from "@clerk/clerk-react";
+
+const frontendApi = process.env.REACT_APP_CLERK_FRONTEND_API;
 
 export default function App() {
+  const navigate = useNavigate();
+
   return (
     <BrowserRouter>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
         <GlobalProvider>
-          <LoadScript
-            googleMapsApiKey={process.env.REACT_APP_GOOGLE_API_KEY as string}
+          <ClerkProvider
+            frontendApi={frontendApi}
+            navigate={(to) => navigate(to)}
           >
-            <AppRoutes />
-            <Alert />
-          </LoadScript>
+            <LoadScript
+              googleMapsApiKey={process.env.REACT_APP_GOOGLE_API_KEY as string}
+            >
+              <AppRoutes />
+              <Alert />
+            </LoadScript>
+          </ClerkProvider>
         </GlobalProvider>
       </ThemeProvider>
     </BrowserRouter>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,61 +7,54 @@ import {
   UserProfile,
 } from "@clerk/clerk-react";
 import React from "react";
-import { Navigate, Route, Routes, useNavigate } from "react-router";
+import { Navigate, Route, Routes } from "react-router";
 import Home from "../pages/Home";
-import { ClerkProvider } from "@clerk/clerk-react";
 import DashboardSection from "../pages/Home/Components/DashboardSection";
 import DestinationsSection from "../pages/Home/Components/DestinationsSection";
 import AchievementsSection from "../pages/Home/Components/AchievementsSection";
 import DestinationSection from "../pages/Home/Components/DestinationSection";
 import { Box } from "@mui/system";
 
-const frontendApi = process.env.REACT_APP_CLERK_FRONTEND_API;
-
 export default function AppRoutes() {
-  const navigate = useNavigate();
-
   return (
-    <ClerkProvider frontendApi={frontendApi} navigate={(to) => navigate(to)}>
-      <Routes>
-        <Route
-          path="/sign-up"
-          element={<SignUp routing="path" path="/sign-up" />}
-        />
-        <Route
-          path="/sign-in"
-          element={<SignIn routing="path" path="/sign-in" />}
-        />
+    <Routes>
+      <Route
+        path="/sign-up"
+        element={<SignUp routing="path" path="/sign-up" />}
+      />
+      <Route
+        path="/sign-in"
+        element={<SignIn routing="path" path="/sign-in" />}
+      />
 
+      <Route
+        path="/dashboard"
+        element={
+          <>
+            <SignedIn>
+              <Home />
+            </SignedIn>
+            <SignedOut>
+              <RedirectToSignIn />
+            </SignedOut>
+          </>
+        }
+      >
+        <Route path="" element={<DashboardSection />} />
+        <Route path="destinations" element={<DestinationsSection />} />
+        <Route path="destinations/:name" element={<DestinationSection />} />
+        <Route path="achievements" element={<AchievementsSection />} />
         <Route
-          path="/dashboard"
+          path="user"
           element={
-            <>
-              <SignedIn>
-                <Home />
-              </SignedIn>
-              <SignedOut>
-                <RedirectToSignIn />
-              </SignedOut>
-            </>
+            <Box sx={{ width: "100%" }}>
+              <UserProfile only="account" />
+            </Box>
           }
-        >
-          <Route path="" element={<DashboardSection />} />
-          <Route path="destinations" element={<DestinationsSection />} />
-          <Route path="destinations/:name" element={<DestinationSection />} />
-          <Route path="achievements" element={<AchievementsSection />} />
-          <Route
-            path="user"
-            element={
-              <Box sx={{ width: "100%" }}>
-                <UserProfile only="account" />
-              </Box>
-            }
-          />
-        </Route>
+        />
+      </Route>
 
-        <Route path="*" element={<Navigate to="/dashboard" />} />
-      </Routes>
-    </ClerkProvider>
+      <Route path="*" element={<Navigate to="/dashboard" />} />
+    </Routes>
   );
 }
